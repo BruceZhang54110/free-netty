17c05c5f34a9a4d04766291f566d98d0c93d6225
zhw54110@163.com <zhw54110@163.com>
2023-09-27 15:10:06 +0800

    first commit

---
+++ src/main/java/com/zhw/free/FreeNettyApplication.java
@@ -0,0 +1,13 @@
+package com.zhw.free;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeNettyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreeNettyApplication.class, args);
+    }
+
+}
